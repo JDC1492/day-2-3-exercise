586a4fe4778ebe19c6d2751eb75a2c9f32db8e2b
Jonathan Cohen <65975460+JDC1492@users.noreply.github.com>
2021-08-13 01:32:32 +0000

    solved

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ age = input("What is your current age?")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+age_int = int(age)
 
+age_int_diff = 90 - age_int
 
+days = age_int_diff * 365
+weeks = age_int_diff * 52
+months = age_int_diff * 12
 
-
+print(f'You have {days} days, {weeks} weeks, and {months} months left.')
 
 
 
